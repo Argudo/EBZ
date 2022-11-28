da58ac412583147b670b54f85772ffc3bb993670
manupruiz <rayerito23@gmail.com>
2022-11-29 00:40:10 +0100

    Placeholders movs y notis
    Creadas las funciones para mostrar los movimientos y las notificaciones, a la espera de retocarlos para los clicklistener  y pasar por parámetro el objeto en concreto

--- Codigo Fuente/src/main/java/es/uca/iw/ebz/views/main/HomeView.java
+++ Codigo Fuente/src/main/java/es/uca/iw/ebz/views/main/HomeView.java
@@ -7,6 +7,7 @@ import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.FlexLayout.FlexDirection;
 import com.vaadin.flow.component.orderedlayout.FlexLayout.FlexWrap;
@@ -26,52 +27,105 @@ public class HomeView extends VerticalLayout{
 	    public HomeView() {
 	        setMargin(false);
 	        setPadding(false);
-	        setSpacing(false);
-	        
+	        setSpacing(true);
+			setWidthFull();
+			setAlignItems(FlexComponent.Alignment.CENTER);
+
 	        VerticalLayout vlMain = new VerticalLayout();
-	        vlMain.setWidthFull();
+	        vlMain.setWidth("80vw");
 	        vlMain.setPadding(false);
-	        vlMain.setMargin(false);
+			vlMain.setSpacing(false);
+	        vlMain.setMargin(true);
+			vlMain.setClassName("box");
 
 	        
 	        //Banner section
 	        //End Banner section
 
 			//Username section
-			Component userName = CreateUserNameBanner("Nombre y apellidos");
+			Component userName = CreateUserNameBanner("Faliyo de San Roque");
 			//End username section
 
 			//Account gallery section
 			//Title accountNumber = new Title("Título de prueba");
-			Component acGallery = ShowAccount("XXXX XXXX XXXX XXXX","100.000,45€");
+			Component acGallery = ShowAccount("XXXX XXXX XXXX XXXX", (float) 100000.45);
 			//End account gallery section
 	        
-	        //Functionalities section
-	        FlexLayout flAccount = new FlexLayout();
-	        flAccount.setWidthFull();
-	        flAccount.setFlexDirection(FlexDirection.ROW);
-	        flAccount.setFlexWrap(FlexWrap.WRAP);
-	        flAccount.setJustifyContentMode(JustifyContentMode.EVENLY);
+	        //Button section
+	        FlexLayout flAccountButtons = new FlexLayout();
+	        flAccountButtons.setWidthFull();
+	        flAccountButtons.setFlexDirection(FlexDirection.ROW);
+	        flAccountButtons.setFlexWrap(FlexWrap.WRAP);
+	        flAccountButtons.setJustifyContentMode(JustifyContentMode.EVENLY);
 
-	        Component cTarjeta = CreateFunctionality("Tarjetas", VaadinIcon.CREDIT_CARD);
-			Component cEstadisticas = CreateFunctionality("Estadísticas", VaadinIcon.BAR_CHART_H);
-	        Component cMovimientos = CreateFunctionality("Movimientos", VaadinIcon.EXCHANGE);
-	        Component cTransferencias = CreateFunctionality("Transferencias", VaadinIcon.MONEY_EXCHANGE);
-	        flAccount.add(
+	        Component cTarjeta = CreateButton("Tarjetas", VaadinIcon.CREDIT_CARD);
+			Component cEstadisticas = CreateButton("Estadísticas", VaadinIcon.BAR_CHART_H);
+	        Component cMovimientos = CreateButton("Movimientos", VaadinIcon.EXCHANGE);
+	        Component cTransferencias = CreateButton("Transferencias", VaadinIcon.MONEY_EXCHANGE);
+	        flAccountButtons.add(
 					cTarjeta,
 					cEstadisticas,
 					cMovimientos,
 					cTransferencias
 	        		);
-	        //End functionalities section
+	        //End button section
 
-			FlexLayout fl;
-
+			//Including name, account information and buttons to the first layout
 			vlMain.add(userName);
 			vlMain.add(acGallery);
-	        vlMain.add(flAccount);
+			vlMain.add(flAccountButtons);
+
+			//Movements and notifications section
+			HorizontalLayout hlAccountMove = new HorizontalLayout();
+			hlAccountMove.setWidthFull();
+			hlAccountMove.setSpacing(true);
+			hlAccountMove.setPadding(false);
+			hlAccountMove.setMargin(true);
+			hlAccountMove.setWidth("80vw");
+
+			//Tengo que crear dos layouts verticales, uno con los primeros movimientos y otro con las primeras notificaciones.
+
+			FlexLayout flAccountMovements = new FlexLayout();
+			flAccountMovements.setWidthFull();
+			flAccountMovements.setFlexDirection(FlexDirection.COLUMN);
+			flAccountMovements.setFlexWrap(FlexWrap.WRAP);
+			flAccountMovements.setJustifyContentMode(JustifyContentMode.EVENLY);
+			flAccountMovements.setClassName("box");
+
+			FlexLayout flAccountNotifications = new FlexLayout();
+			flAccountNotifications.setWidthFull();
+			flAccountNotifications.setFlexDirection(FlexDirection.COLUMN);
+			flAccountNotifications.setFlexWrap(FlexWrap.WRAP);
+			flAccountNotifications.setJustifyContentMode(JustifyContentMode.EVENLY);
+			flAccountNotifications.setClassName("box");
+
+			Component cMovement1 = CreateMovement();
+			Component cMovement2 = CreateMovement();
+			Component cMovement3 = CreateMovement();
+
+			Component cNotification1 = CreateNotification();
+			Component cNotification2 = CreateNotification();
+			Component cNotification3 = CreateNotification();
+
+			flAccountMovements.add(
+					cMovement1,
+					cMovement2,
+					cMovement3);
+
+			flAccountNotifications.add(
+					cNotification1,
+					cNotification2,
+					cNotification3);
+
+			hlAccountMove.add(
+					flAccountMovements,
+					flAccountNotifications);
+
+			//End movements and notifications section
 	        
 	        add(vlMain);
+			add(hlAccountMove);
+
 	       
 	    }
 
@@ -82,12 +136,10 @@ public class HomeView extends VerticalLayout{
 	btnLogIn
     			);
     	vlMid.setAlignItems(FlexComponent.Alignment.CENTER);
-    	btnLogIn.addClickListener( click -> {
-		btnLogIn.getUI().ifPresent(ui ->
-				ui.navigate("home"));
-	});*/
+    	btnLogIn.addClickListener( click -> { btnLogIn.getUI().ifPresent(ui ->ui.navigate("home")); });
+				*/
 	    
-	    private Component CreateFunctionality(String sName, VaadinIcon vI) {
+	    private Component CreateButton(String sName, VaadinIcon vI) {
 	    	VerticalLayout vlMain = new VerticalLayout();
 	    	vlMain.setAlignItems(FlexComponent.Alignment.CENTER);
 	    	vlMain.setSpacing(false);
@@ -108,14 +160,16 @@ public class HomeView extends VerticalLayout{
 	    }
 
 		//Componente de prueba para mostrar la información de la cuenta.
-		private Component ShowAccount(String acNumber, String acBalance) {
+		private Component ShowAccount(String acNumber, float acBalance) {
 			VerticalLayout vlMain = new VerticalLayout();
 			vlMain.setAlignItems(Alignment.CENTER);
 			vlMain.setSpacing(false);
 			vlMain.setWidth("100%");
 
+			String _sAux = acBalance + "€";
+
 			H2 _acNumber = new H2(acNumber);
-			H3 _acBalance = new H3(acBalance);
+			H3 _acBalance = new H3(_sAux);
 			vlMain.add(
 					_acNumber,
 					_acBalance
@@ -126,6 +180,7 @@ public class HomeView extends VerticalLayout{
 		}
 
 		//Componente para mostrar el nombre del usuario en la vista principal.
+		//Falta arreglar la situación del nombre
 		private Component CreateUserNameBanner(String userName){
 			VerticalLayout vlMain = new VerticalLayout();
 			vlMain.setAlignItems(Alignment.CENTER);
@@ -137,6 +192,39 @@ public class HomeView extends VerticalLayout{
 			vlMain.add(_userName);
 
 			return vlMain;
+
+		}
+
+		private Component CreateMovement(){
+			VerticalLayout vlMain = new VerticalLayout();
+			vlMain.setAlignItems(Alignment.CENTER);
+			vlMain.setSpacing(false);
+			vlMain.setWidth("min-width");
+
+			Paragraph _mv1 = new Paragraph("Un farzerío total y absoluto");
+
+			//_mv1.addClickListener();
+
+			vlMain.add(
+					_mv1);
+
+			return vlMain;
+
+
+		}
+
+		private Component CreateNotification(){
+			VerticalLayout vlMain = new VerticalLayout();
+			vlMain.setAlignItems(Alignment.CENTER);
+			vlMain.setSpacing(false);
+			vlMain.setWidth("min-width");
+
+			Paragraph _mv1 = new Paragraph("Y la gente lo sabe");
+
+			vlMain.add(_mv1);
+
+			return vlMain;
+
 
 		}
 
