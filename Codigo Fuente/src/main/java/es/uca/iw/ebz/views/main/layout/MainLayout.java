7eb3e3528299aa1b92aa88b5c831d9f4186b13fa
saikora11 <pedro.garciaromera@alum.uca.es>
2022-12-21 13:30:19 +0100

    Logout
    Logout Implementado

--- Codigo Fuente/src/main/java/es/uca/iw/ebz/views/main/layout/MainLayout.java
+++ Codigo Fuente/src/main/java/es/uca/iw/ebz/views/main/layout/MainLayout.java
@@ -21,10 +21,15 @@ import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.tabs.TabsVariant;
 import com.vaadin.flow.router.RouterLink;
 import es.uca.iw.ebz.views.main.HomeView;
+import es.uca.iw.ebz.views.main.Security.AuthenticatedUser;
 import es.uca.iw.ebz.views.main.TarjetaView;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class MainLayout extends AppLayout{
-	
+	@Autowired
+	private AuthenticatedUser _authenticatedUser;
+
+
 	public MainLayout() {
 		CreateHeader();
 		
@@ -40,6 +45,9 @@ public class MainLayout extends AppLayout{
     	Button btnSignOut = new Button();
     	Icon icon = new Icon(VaadinIcon.SIGN_OUT);
     	btnSignOut.getElement().appendChild(icon.getElement());
+		btnSignOut.addClickListener(e -> {
+			_authenticatedUser.logout();
+		});
         H1 logo = new H1("EBZ");
         logo.addClassNames("text-l", "m-m");
         hlContent.setAlignItems(FlexComponent.Alignment.CENTER);
