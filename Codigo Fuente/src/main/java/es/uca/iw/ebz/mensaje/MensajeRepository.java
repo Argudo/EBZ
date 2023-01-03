9763eed9bea341b5e381d7bc66f176f2b742efd0
manupruiz <rayerito23@gmail.com>
2023-01-03 19:14:18 +0100

    Consulta avanzada, mensaje creada y terminada a falta de pruebas

---
+++ Codigo Fuente/src/main/java/es/uca/iw/ebz/mensaje/MensajeRepository.java
@@ -0,0 +1,13 @@
+package es.uca.iw.ebz.mensaje;
+
+import es.uca.iw.ebz.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface MensajeRepository extends JpaRepository<Mensaje, UUID> {
+    List<Mensaje> findBy_autor(Usuario usuario);
+}
